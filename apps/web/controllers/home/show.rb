a0826cbabb15924cecc272b89b19df9c2d2d2327
Nicolas <nfilzi.webservices@gmail.com>
2017-05-28 19:57:58 +0900

    Finish home layout

--- apps/web/controllers/home/show.rb
+++ apps/web/controllers/home/show.rb
@@ -1,8 +1,11 @@
 module Web::Controllers::Home
   class Show
     include Web::Action
+
+    expose :total_trips_count
     
     def call(params)
+      @total_trips_count = TripRepository.new.count
     end
   end
 end
