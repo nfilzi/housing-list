930ce9db97e39054547da223e7efd3c8d4c3f279
Cecile Veneziani <contact@cecilitse.org>
2017-08-19 23:03:45 +0200

    Fix trip page when there is no housing

--- lib/housing_list/entities/trip_housing_stats.rb
+++ lib/housing_list/entities/trip_housing_stats.rb
@@ -1,7 +1,7 @@
 class TripHousingStats
   attr_reader :housings_count, :total_price_min, :total_price_max, :trip_id
 
-  def initialize(trip_id:, housings_count: ,total_price_avg:, total_price_min:, total_price_max:)
+  def initialize(trip_id:, housings_count: 0, total_price_avg: 0, total_price_min: 0, total_price_max: 0)
     @trip_id         = trip_id
     @housings_count  = housings_count
     @total_price_avg = total_price_avg
