7be6ca645d1b4302d0ff01a8b3b6a58f0ee9f3c0
Cecile Veneziani <contact@cecilitse.org>
2017-10-01 10:37:45 +0200

    Fix ongoing / completed trips views
    Do not make then view inherit from index, it breaks in production

--- apps/web/views/trips/completed.rb
+++ apps/web/views/trips/completed.rb
@@ -1,7 +1,16 @@
-require_relative 'index'
-
 module Web::Views::Trips
-  class Completed < Web::Views::Trips::Index
+  class Completed
+    include Web::View
+    include Web::Helpers::DateFormatter
+
     template 'trips/index'
+
+    def days_before_trip(trip)
+      (trip.starting_on - Date.today).to_i
+    end
+
+    def format_trip_date(date)
+      format_date(date, format: '%b %e', ordinal_indicator: true)
+    end
   end
 end
