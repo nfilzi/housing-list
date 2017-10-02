9bd900f30112d910d20f3316f3e7155b1df9d11d
Cecile Veneziani <contact@cecilitse.org>
2017-10-02 20:31:39 +0200

    Add user picture to profile edition

--- apps/web/views/profile/edit.rb
+++ apps/web/views/profile/edit.rb
@@ -1,6 +1,12 @@
+require_relative '../../presenters/user_presenter'
+
 module Web::Views::Profile
   class Edit
     include Web::View
+
+    def profile
+      UserPresenter.new(locals[:profile])
+    end
 
     def sticky_navbar
       true
