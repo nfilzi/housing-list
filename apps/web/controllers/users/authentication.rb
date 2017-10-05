be07194c4900ef62ecf6913022a374da9200a050
Cecile Veneziani <contact@cecilitse.org>
2017-10-05 16:41:33 +0200

    Fix access to trip page when user isn't signed in

--- apps/web/controllers/users/authentication.rb
+++ apps/web/controllers/users/authentication.rb
@@ -14,17 +14,21 @@ module Web
 
       def authenticate_user!
         unless user_signed_in?
-          session[:user_return_to] = params.env['REQUEST_PATH']
+          store_current_location
           redirect_to(routes.user_sign_in_path)
         end
       end
 
-      def user_signed_in?
-        !!current_user
-      end
-
       def current_user
         @current_user ||= UserRepository.new.find(session[:user_id]) if session[:user_id]
+      end
+
+      def store_current_location
+        session[:user_return_to] = params.env['REQUEST_PATH']
+      end
+
+      def user_signed_in?
+        !!current_user
       end
     end
   end
