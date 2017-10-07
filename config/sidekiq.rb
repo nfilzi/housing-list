bac29379d4c2375c10f138ef5694fc33a3368143
Nicolas Filzi <nfilzi.webservices@gmail.com>
2017-10-07 13:01:48 +0200

    Decrease sidekiq client and server connections count

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') }
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 7 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL') }
+  config.redis = { url: ENV.fetch('REDIS_URL'), size: 3 }
 end
