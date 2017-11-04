9fe3deed7c347a9e114164ea9f6ca98a1aefb2c7
Cecile Veneziani <contact@cecilitse.org>
2017-11-05 00:30:43 +0100

    Update styleguide to new housing card
    - Add an id to trip and housing so that action link works fine.
    - Add circle button to the list of buttons.

--- apps/web/views/styleguide/show.rb
+++ apps/web/views/styleguide/show.rb
@@ -4,8 +4,14 @@ module Web::Views::Styleguide
     include Web::Helpers::DateFormatter
     include Web::Helpers::TextFormatter
 
+    # Housing component
+
     def housing
       HousingPresenter.new(locals[:housing])
+    end
+
+    def housing_authorization(housing)
+      Web::Trips::Housings::Authorization.new(current_user, trip, housing)
     end
   end
 end
