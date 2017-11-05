b82f84a947b534533f7c90fcbbe139a85bb20633
Cecile Veneziani <contact@cecilitse.org>
2017-11-05 21:34:57 +0100

    Update trip housing stats to ignore dismissed housings

--- lib/housing_list/repositories/trip_repository.rb
+++ lib/housing_list/repositories/trip_repository.rb
@@ -5,8 +5,9 @@ class TripRepository < Hanami::Repository
   end
 
   def all_for_organizer_by_status(organizer_id, status)
+    # No ROM here, we have to use a specific left join
     all_trips = trips_with_stats.
-      join(:trip_organizers).
+      join_table(:inner, :trip_organizers, trip_id: Sequel.qualify(:trips, :id)).
       where(organizer_id: organizer_id)
 
     case status
@@ -25,7 +26,7 @@ class TripRepository < Hanami::Repository
         order { ending_on.desc }
     end
 
-    return all_trips.as(TripWithStats).to_a
+    return all_trips.to_a.lazy.map { |attributes| TripWithStats.new(attributes) }
   end
 
   def create_with_organizer(data)
@@ -33,10 +34,12 @@ class TripRepository < Hanami::Repository
   end
 
   def find_with_stats(id)
-    trips_with_stats.
+    # No ROM here, we have to use a specific left join
+    attributes = trips_with_stats.
       where(Sequel.qualify(:trips, :id) =>  id).
-      as(TripWithStats).
-      one
+      first
+
+    return TripWithStats.new(attributes)
   end
 
   def count
@@ -46,16 +49,25 @@ class TripRepository < Hanami::Repository
   private
 
   def trips_with_stats
-    relations[:trips].
-      left_join(relations[:housings]). # auto qualifies the columns
-      select_append {
+    # No ROM here, we have to implement a specific left join
+    housing_stats = relations[:housings].select {
         [
+          :trip_id,
           int::count(Sequel.qualify(:housings, :id)).as(:housings_count),
           int::avg(:total_price).as(:total_price_avg),
           int::min(:total_price).as(:total_price_min),
           int::max(:total_price).as(:total_price_max)
         ]
       }.
-      group(:id)
+      where(dismissed: false).
+      group(:trip_id).
+      order(:trip_id)
+
+    relations[:trips].
+      dataset.
+      select_all(:trips).
+      select_append(:housings_count, :total_price_avg, :total_price_min, :total_price_max).
+      join_table(:left, housing_stats.dataset, trip_id: :id).
+      order(:id)
   end
 end
