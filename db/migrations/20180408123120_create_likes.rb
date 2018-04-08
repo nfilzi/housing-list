e9a8ed9731fc1413dfa5ced83a71c3d0aaef7e64
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-04-08 14:44:03 +0200

    Add migration to create likes table

---
+++ db/migrations/20180408123120_create_likes.rb
@@ -0,0 +1,15 @@
+Hanami::Model.migration do
+  change do
+    create_table :likes do
+      primary_key :id
+      foreign_key :housing_id, :housings, null: false, on_delete: :cascade
+      foreign_key :user_id,    :users,    null: false, on_delete: :cascade
+
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+
+
+      index [:housing_id, :user_id], unique: true
+    end
+  end
+end
