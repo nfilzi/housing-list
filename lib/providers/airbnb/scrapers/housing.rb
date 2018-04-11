951850797a57ad20ec5bb2b6562072ac01a8e76f
Cecile Veneziani <contact@cecilitse.org>
2018-04-12 00:26:12 +0200

    Refactor providers scraping libraries
    Move all the scrapers into a providers folder and first, start
    namespacing by provider.

---
+++ lib/providers/airbnb/scrapers/housing.rb
@@ -0,0 +1,101 @@
+module Providers
+  module Airbnb
+    module Scrapers
+      class Housing
+        private
+        attr_reader :url, :browser
+
+        public
+
+        def initialize(url:)
+          @url = url
+        end
+
+        def scrape
+          init_scraping_engine!(driver: :poltergeist) # or :selenium
+
+          browser.visit(url_with_currency)
+
+          return build_housing_attributes
+        end
+
+        private
+
+        def url_with_currency
+          url + "&currency=#{find_currency}"
+        end
+
+        def find_currency
+          # We can add &currency=#{currency} to housing url query string to get euro price for now
+          # when currency will be added to trip
+          # housing.trip.currency || "EUR"
+          "EUR"
+        end
+
+        def build_housing_attributes
+          data = extract_airbnb_housing_data
+          total_price = browser.find("#book_it_form").text.match(/Total .(\d+)/)[1].to_i
+
+          return {
+            title:       data["sectioned_description"]["name"],
+            description: data["sectioned_description"]["description"],
+            total_price: total_price,
+            provider_id: data["id"],
+            picture_url: data["photos"].first["large"],
+            raw_data:    data
+          }
+        end
+
+        def extract_airbnb_housing_data
+          raw_data = browser.all("body script", visible: false)[5].text(:all)
+          JSON.parse(raw_data[4..-4])["bootstrapData"]["reduxData"]["homePDP"]["listingInfo"]["listing"]
+        end
+
+        def init_browser(driver)
+          @browser = Capybara.current_session
+
+          # To get the desktop version for the scraped page, in order to get access
+          # to the flat booking form
+          if driver == :poltergeist
+            browser.driver.resize(3072, 2304)
+          else
+            browser.driver.resize_window_to(browser.driver.current_window_handle, 3072, 2304)
+          end
+        end
+
+        def init_capybara(driver)
+          driver == :poltergeist ? init_capybara_with_poltergeist : init_capybara_with_selenium
+        end
+
+        def init_capybara_with_poltergeist
+          require 'capybara/poltergeist'
+
+          Capybara.register_driver :poltergeist do |app|
+            Capybara::Poltergeist::Driver.new(app, phantomjs: Phantomjs.path, js_errors: false)
+          end
+
+          Capybara.default_driver = :poltergeist
+        end
+
+        def init_capybara_with_selenium
+          # To debug with a real browser, if needed
+          Capybara.register_driver :selenium do |app|
+            Capybara::Selenium::Driver.new(app, browser: :chrome)
+          end
+
+          Capybara.javascript_driver = :chrome
+
+          Capybara.configure do |config|
+            config.default_max_wait_time = 10 # seconds
+            config.default_driver        = :selenium
+          end
+        end
+
+        def init_scraping_engine!(driver:)
+          init_capybara(driver)
+          init_browser(driver)
+        end
+      end
+    end
+  end
+end
---
+++ lib/providers/booking/scrapers/hotel.rb
@@ -0,0 +1,98 @@
+module Providers
+  module Booking
+    module Scrapers
+      class Hotel
+        private
+        attr_reader :url, :browser
+
+        public
+
+        def initialize(url:)
+          @url = url
+        end
+
+        def scrape
+          init_capybara
+          @browser = Capybara.current_session
+          browser.visit(url)
+
+          return build_hotel_attributes
+        end
+
+        private
+
+        def build_hotel_attributes
+          data     = extract_hotel_data
+          raw_data = extract_raw_data
+          hotel_id = raw_data.delete(:hotel_id)
+
+          return {
+            title:       data[:title],
+            description: data[:description],
+            provider_id: hotel_id,
+            picture_url: data[:picture_url],
+            raw_data:    raw_data
+          }
+        end
+
+        def extract_hotel_data
+          title_selector  = ".hp__hotel-name"
+          title           = browser.find(title_selector).text.strip
+
+          description_selector  = ".hp-description .hp_desc_main_content #summary p"
+          description           = browser.all(description_selector).map(&:text).map(&:strip).join("\n").strip
+          picture_url           = extract_picture_url
+
+          {
+            title:       title,
+            description: description,
+            picture_url: picture_url
+          }
+        end
+
+        def extract_picture_url
+          pictures_selector = ".bh-photo-grid .active-image"
+          pictures_links    = browser.all(pictures_selector)
+
+          if pictures_links.any?
+            return pictures_links.first["href"]
+          else
+            pictures_selector = ".hp-gallery .slick-track img"
+            pictures_images   = browser.all(pictures_selector)
+
+            return pictures_images.first["src"]
+          end
+        end
+
+        def extract_raw_data
+          basic_raw_data  = {}
+          hotel_id        = nil
+          hotel_id_regexp = /b_hotel_id:\s'(?<hotel_id>\d+)'/
+
+          scripts = browser.all("head script", visible: false)
+
+          scripts.each do |script|
+            raw_script = script["innerHTML"]
+
+            if basic_raw_data.empty? && raw_script.include?("priceRange")
+              basic_raw_data = JSON.parse(raw_script.gsub("\n", ""))
+            elsif hotel_id.nil? && raw_script =~ hotel_id_regexp
+              hotel_id = raw_script.match(hotel_id_regexp)[:hotel_id].to_i
+            end
+          end
+
+          return basic_raw_data.merge(hotel_id: hotel_id)
+        end
+
+        def init_capybara
+          require 'capybara/poltergeist'
+          Capybara.register_driver :poltergeist do |app|
+            Capybara::Poltergeist::Driver.new(app, phantomjs: Phantomjs.path, js_errors: false)
+          end
+
+          Capybara.default_driver = :poltergeist
+        end
+      end
+    end
+  end
+end
